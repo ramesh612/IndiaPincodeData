c49888afc55de4de424e9e81a7d2afd320f8f99b
Ramesh Balasubramanian <rbalasubramanian@verisign.com>
2018-04-02 14:21:47 -0400

    initial version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 IndiaPincodeData
 ================
 
-R Dataset that contains the latest pincode data for India
+Dataset that contains the latest pincode data for India
 
 For an idea about the strcture of the data, see http://www.mapsofindia.com/pincode/
---
+++ india-pincodes.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import requests
+from BeautifulSoup import BeautifulSoup
+import io, json
+
+url = 'https://www.mapsofindia.com/pincode/india'
+
+def get_soup(url):
+	response = requests.get(url)
+	# print response.content
+	# print response.status_code
+	soup = BeautifulSoup(response.content)
+	return soup
+
+def get_content(url):
+	soup = get_soup(url)
+	# print soup.prettify()
+	urls_dict = {}
+	for a in soup.findAll('a'):
+		if '/pincode/india/' in a['href']:
+			urls_dict[a.getString()] = a['href']
+	return urls_dict
+
+def get_pincode(url):
+	soup = get_soup(url)
+	for v in soup.findAll('td'):
+		if v.getText().isdigit():
+			return int(v.getText())
+
+output = []
+states_url = get_content(url)
+for state in states_url.keys():
+	print state, ":", states_url[state] 
+	output_dict = {}
+	output_dict['state'] = state
+	districts_url = get_content(states_url[state])
+	output_districts = []
+	for district in districts_url.keys():
+		print '\t', district, ':', districts_url[district]
+		output_district = {}
+		output_district['district'] = district
+		locations_url = get_content(districts_url[district])
+		output_locations = []
+		for location in locations_url.keys():
+			pincode = get_pincode(locations_url[location])
+			print '\t\t', location, ':', pincode, ':', locations_url[location]
+			output_location = {}
+			output_location['location'] = location
+			output_location['pincode'] = pincode
+			output_locations.append(output_location)
+		output_district['locations'] = output_locations
+		output_districts.append(output_district)
+	output_dict['districts'] = output_districts
+	output.append(output_dict)
+data = {'country': 'india', 'states': output}
+with io.open('india-pincode.txt', 'w', encoding='utf-8') as f:
+	f.write(json.dumps(data, ensure_ascii=False))
